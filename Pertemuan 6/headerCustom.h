b017cbcfd5c5252be3f40ebe60a2efe7625c0c58
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:44:00 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/headerCustom.h
+++ Pertemuan 6/headerCustom.h
@@ -1,4 +1,5 @@
-#define L <<
+// mendefinisikan perintah yang sudah ada menjadi perintah yang diinginkan, gtw gimana jelasinnya
+#define L << // menggunakan "L" untuk mendefinisikan "<<"
 #define u using namespace std;
 #define o cout
 #define a endl
