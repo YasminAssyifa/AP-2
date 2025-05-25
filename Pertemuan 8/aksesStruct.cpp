d285af5e0167812e47021ea2bc0f1e02947d3e41
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:58:05 +0700

    Pertemuan 5 : Struct & Class

--- Pertemuan 8/aksesStruct.cpp
+++ Pertemuan 8/aksesStruct.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-struct Mahasiswa
+struct Mahasiswa // kumpulan variabel dengan tipe data yang berbeda
 {
     string nama;
     int umur;
@@ -11,8 +11,8 @@ struct Mahasiswa
 
 int main()
 {
-    Mahasiswa mhs1;
-    mhs1.nama = "Yasmin";
+    Mahasiswa mhs1; // struct Mahasiswa didefinisikan sebagai mhs1
+    mhs1.nama = "Yasmin"; // dari struct mhs1 dengan variabel nama
     mhs1.umur = 18;
     mhs1.ipk = 4.00;
 
@@ -20,6 +20,7 @@ int main()
     // cout << "Umur = " << mhs1.umur << endl;
     // cout << "IPK = " << mhs1.ipk << endl;
 
+    // menampilkan isi struct menggunakan pointer dan tanda (->)
     Mahasiswa *ptrMhs = &mhs1;
     cout << "Nama = " << ptrMhs->nama << endl;
     cout << "Umur = " << ptrMhs->umur << endl;
