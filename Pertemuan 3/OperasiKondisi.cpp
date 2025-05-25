1d8244289e2a1bb17ea4ac95aebabda89959af5e
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 13:43:58 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/OperasiKondisi.cpp
+++ Pertemuan 3/OperasiKondisi.cpp
@@ -13,7 +13,7 @@ int main()
     // //if statement
     // if (nilai <= 65)
     // {
-    //     cout << "Anda tidak lulus!" << endl;
+    //     cout << "Anda tidak lulus!" << endl;  // program/line akan berjalan jika nilai lebih besar sama dengan 65
 
     // // if - else statement
     // //if statement
@@ -21,7 +21,7 @@ int main()
     // {
     //     cout << "Anda tidak lulus!" << endl;
     // }
-    // else { cout << "Anda Lulus!" << endl;}
+    // else { cout << "Anda Lulus!" << endl;}  // jika statement nilai <= 65 tidak terpenuhi, maka akan lompat ke else dan menjalankan line lain.
 
     // // if - else - if statement
     // if (nilai == 100)
@@ -34,7 +34,9 @@ int main()
     // }
     // else { cout << "Anda Lulus!" << endl;}
 
-    // //nested if
+    // if else digunakan jika ada beberapa statement atau requirement dari sebuah program.
+
+    // //nested if atau else didalam if.
     // if (nilai <= 65)
     // {
     //     cout << "Anda tidak lulus" << endl;
@@ -49,16 +51,17 @@ int main()
     // switch case
     // switch (nilai)
     // {
-    //     case 1: cout << "Senin" << endl; break;
+    //     case 1: cout << "Senin" << endl; break; //case, jika var "nilai" diinput dengan 1, maka akan menjalankan line ini.
     //     case 2: cout << "Selasa" << endl; break;
     //     case 3: cout << "Rabu" << endl; break;
     //     case 4: cout << "Kamis" << endl; break;
     //     case 5: cout << "Jumat" << endl; break;
     //     case 6: cout << "Sabutu" << endl; break;
     //     case 7: cout << "Minggu" << endl; break;
-    //     default : cout << " Inputan tidak valid" << endl; break; goto back;
+    //     default : cout << " Inputan tidak valid" << endl; break; goto back; // untuk goto itu sendiri, digunakan untuk melompat ke bagian dari suatu program.
     // }
 
+    // case menggunakan "..." sebagai rentan nilai (range)
     switch (nilai)
     {
     case 85 ... 100:
@@ -84,7 +87,10 @@ int main()
         break;
     }
 
+    //Pada operasi kondisi switch, default berguna sebagai pilihan lain (else) jika semua requirement tidak terpenuhi.
+
     // a > 5 ? 9 : 6
+    // apakah nilai a lebih besar dari 5? kalau iya, maka akan keluar outputan 9 dan jika tidak maka akan keluar 6.
     string checkNum = (nilai % 2 == 0)? "Genap" : "Ganjil";
     cout << nilai << "Tuh bilangan" << checkNum << "sih" << endl;
 
