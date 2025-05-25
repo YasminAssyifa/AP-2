04f8cc2a747c00ad04e3c1ac96ef84d9486fa3a5
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:40:49 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/fungsi.cpp
+++ Pertemuan 6/fungsi.cpp
@@ -13,18 +13,18 @@ int tambah(int a, int b)
     return a+b;
 }
 
-// fungsi overlord
+// fungsi overlord, fungsi dengan nama yang sama tapi tipe data yang berbeda
 int kali(int a, int b)
 {
     return a*b;
 }
 
-double kali( double a, double b)
+double kali( double a, double b) // fungsi dengan tipe data double
 {
     return a*b;
 }
 
-// fungsi rekursif
+// fungsi rekursif, atau fungsi yang memanggil fungsi lain atau dirinya sendiri. biasannya digunakan untuk pangkat dan faktorial.
 int faktorial (int n)
 {
     if (n == 0 || n == 1)
@@ -43,7 +43,7 @@ int main()
     int x = 5, y = 3;
 
     // menggunakan fungsi dgn nilai balikan
-    int hasilTambah = tambah(x,y);
+    int hasilTambah = tambah(x,y); // memanggil fungsi tambah yang nanti akan memberikan nilai balik ke variabel hasilTambah
 
     // menggunakan fungsi overload
     int hasilKaliInt = kali(x,y);
@@ -53,6 +53,7 @@ int main()
     cout << "Hasil perkalian (double) : " << hasilKaliDouble << endl;
 
     // menggunakan fungsi rekursif
+    // mencari faktorial dari 5 menggunakan fungsi rekursif
     int angka = 5;
     cout << "Faktorial dari " << angka << " adalah: "<< faktorial(angka) << endl;
 }
