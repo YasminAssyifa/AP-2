8fa0fb6337da047cfce0840cdd219919811a5632
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:28:25 +0700

    Pertemuan 6 : Fungsi & Header

--- Pertemuan 6/BelajarFungsi.cpp
+++ Pertemuan 6/BelajarFungsi.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama)
+void sapa(string nama) // fungsi sapa
 {
     cout << "Halo " << nama << " ! Selamat Datang di AP 2" << endl;
 }
@@ -10,5 +10,5 @@ int main ()
 {
     string NamaPengguna = "Alya";
 
-    sapa(NamaPengguna);
+    sapa(NamaPengguna); // memanggil fungsi sapa
 }
