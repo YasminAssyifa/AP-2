a91c47077d99fa3644ea5be9a7c3a1539c75b562
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:06:26 +0700

    Pertemuan 5 : Array & Vektor

--- Pertemuan 5/vector.cpp
+++ Pertemuan 5/vector.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     // Vector Declaration & Initialization
-    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
+    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"}; //mengisi vector
 
     // Ascending Element of Vector
     // for (int i = 0; i < nama_karyawan.size(); i++)
@@ -19,7 +19,7 @@ int main()
     // }
 
     // Add data to Vector 
-    nama_karyawan.push_back("Imam");
+    nama_karyawan.push_back("Imam"); // memasukkan data/item kedalam vektor dengan index terakhir
 
     // for (int i = 0; i < nama_karyawan.size(); i++)
     // {
@@ -27,8 +27,8 @@ int main()
     // }
 
     // Delete data from Vector
-    nama_karyawan.pop_back();
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
+    nama_karyawan.pop_back(); // menghapus vektor index terakhir
+    nama_karyawan.erase(nama_karyawan.begin() + 3); // menghapus item di posisi ke 3
 
     for (int i = 0; i < nama_karyawan.size(); i++)
     {
