2eef1ec9d16f6408ff7220d64bc4c88baa86af1f
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 13:02:55 +0700

    Pertemuan  2 : Penggunaan Operator

--- Pertemuan 2/Latihan.cpp
+++ Pertemuan 2/Latihan.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    const float phi = 3.14;
+    const float phi = 3.14; // variabel konstan yang tidak bisa diubah saat program berjalan
     float jarijari, Volume, Luas, jari;
 
     cout << "Masukkan Jari-Jari Bola : ";
     cin >> jarijari;
 
-    jari = jarijari * jarijari;
-    Volume = (float)4/3 * phi * jari * jarijari;
+    jari = jarijari * jarijari; //menggunakan operator matekamatika "*" untuk perkalian.
+    Volume = (float)4/3 * phi * jari * jarijari; // "(float)" digunakan untuk menggubah tipe data dari hasil variabel itu sendiri
     Luas = 4 * phi * jari;
 
     cout << "Volume Bola = " << Volume << endl;
