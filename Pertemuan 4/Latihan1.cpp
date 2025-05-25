bbcb46f38def4eca3d2746907caacd108178b8f9
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 15:18:48 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/Latihan1.cpp
+++ Pertemuan 4/Latihan1.cpp
@@ -9,10 +9,11 @@ int main()
     system("CLS");
     cout << "Masukkan kalimat : "; getline(cin, kalimat);
 
+    // Perulangan menggunakan length, untuk i = 0, akan berjalan jika i lebih kecil dari panjang variabel kalimat, dan akan melakukan i increment saat 1 loop selesai
     for ( i = 0 ; i < kalimat.length(); i++){
         kalimat[i] = toupper(kalimat[i]);
     }
 
     cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
-    return 0;
+    return 0; // mengembalikan nilai
 }
