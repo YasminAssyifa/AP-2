c63a79491baeb93e4f5050a3869d4155f5c90b8a
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 16:03:31 +0700

    Pertemuan 5 : Array & Vektor

--- Pertemuan 5/array.cpp
+++ Pertemuan 5/array.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
-#include <array>
+#include <string> // header untuk string
+#include <array> // header untuk array
 using namespace std;
 
 int main()
@@ -10,8 +10,8 @@ int main()
     /* 2 Cara Membuat Array */
 
     // Cara 1 : Array Kosong
-    // string nama[5];
-    // nama[0]="Alya";
+    // string nama[5]; // array yang berisi 5, dimulai dengan index 0
+    // nama[0]="Alya"; // array untuk index 0
     // nama[1]="Parul";
     // nama[2]="Syukron";
     // nama[3]="Aurick";
@@ -31,12 +31,14 @@ int main()
     //Menggunakan Looping
 
     //Ascending array in looping
+    // mengeluarkan array dari index 0 sampai 4
     // for (int i = 0; i < 5; i++)
     // {
     //     cout<<nama[i]<<endl;
     // }
 
     //Descending array in looping
+    // menggeluarkan array dari index 4 sampai 0
     // for (int i = 4; i >= 0; i--)
     // {
     //     cout<<nama[i]<<endl;
@@ -46,6 +48,7 @@ int main()
     // 1 3 5
     // 2 4 6
 
+    // matriks 2 x 3
     // int matrix[2][3] = {{1, 3, 5},{2, 4 ,6}};
     // for( int i = 0; i < 2; i++)
     // {
@@ -56,19 +59,20 @@ int main()
     //     cout << endl;
     // }
 
+    // matrix 4 x 6
     // int matrix[4][6] = {{1, 2, 3, 4, 5, 6}, {7, 8, 9, 10, 11, 12}, {13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23, 24}};
     // for( int i = 0; i < 4; i++)
     // {
     //     for ( int j = 0; j < 6; j++)
     //     {
-    //         cout << matrix[i][j] << "\t";
+    //         cout << matrix[i][j] << "\t"; // \t untuk membuat rapi / bertabel
     //     }
     //     cout << endl;
     // }
 
     // String Array for characters
     // string nama = "Kiel";
-    // // cout << nama[0] << endl;
+    // // cout << nama[0] << endl; //mengeluarkan karakter index 0 dari "kiel" yaitu "k"
     // // cout << nama[2] << endl;
 
     // for (int i = 0; i < nama.length(); i++)
@@ -81,20 +85,20 @@ int main()
 
     /* 1. s1 = s2*/
     // s1 = s2;
-    // cout << "s1 =" << s1 << endl;
+    // cout << "s1 =" << s1 << endl; // s1 = s2 = "world"
 
     /* 2. s1 + s2 */
-    // cout << "Hasil : " << s1+s2; << endl;
+    // cout << "Hasil : " << s1+s2; << endl; // "HelloWorld"
 
     /* 3. s1.length() */
     // cout << s1.length() << endl;
-    // cout << (s1+s2).length() << endl;
+    // cout << (s1+s2).length() << endl; // length "hello" 5 + length "world" 5 = 10
 
     /* 4. s1.substr(n, m) */
-    // cout << s1.substr(2, 4) << endl;
-    // cout << (s1+s2).substr(5, 3) << endl;
+    // cout << s1.substr(2, 4) << endl; // mengambil karakter dari index 2 sampai banyaknya 4. "llo"
+    // cout << (s1+s2).substr(5, 3) << endl; // mengambil dari index 5 sampai banyak 3 "Wor"
 
-    // Operator sizeof
+    // Operator sizeof, untuk mengetahui ukuran bit dari suatu tipe data
     // int angka = 10;
     // cout << sizeof(angka);
 
@@ -108,11 +112,13 @@ int main()
     // array<float, 5> nilai = {87.5, 90, 100, 95.7, 78.3};
     // cout << "Nilai : ";
 
+    // melakukan pengulangan untuk i = 0, dengan syarat i lebih kecil dari ukuran/size nilai. mengeluarkan nilai index i
     // for(int i = 0; i < nilai.size(); i++)
     // {
     //     cout << nilai[i] << " ";
     // }
 
+    // mengeluarkan seluruh anggota array nilai
     // for (float n : nilai)
     // {
     //     cout << n << " ";
