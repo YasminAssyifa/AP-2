0d8a641718d3ba295dc7061df6c086c94d684cbf
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 13:47:11 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/latihancpp.cpp
+++ Pertemuan 3/latihancpp.cpp
@@ -8,16 +8,16 @@ int main ()
     cout << "Masukkan bilangan bulat: ";
     cin >> nilai;
 
-    if ((nilai % 5 == 0) && (nilai % 10 == 0))
+    if ((nilai % 5 == 0) && (nilai % 10 == 0)) // jika nilai mod 5 sama dengan 0 DAN nilai mod 10 sama dengan 0, program akan berjalan. kedua statement harus true.
     {
         cout << "Bilangan ini habis dibagi 5 dan 10" << endl;
     }
-    else if  (nilai % 5 == 0)
+    else if  (nilai % 5 == 0) // jika nilai mod 5 sama dengan 0, program akan berjalan.
     {
         cout << "Bilangan ini habis dibagi 5" << endl;
     }
-    else if  (nilai % 10 == 0)
+    else if  (nilai % 10 == 0) // jika nilai mod 10 sama dengan 0 maka program akan berjalan.
     {
         cout << "Bilangan ini habis dibagi 10" << endl;
-    } else { cout << "ERROR 101" << endl; }
+    } else { cout << "ERROR 101" << endl; } // jika semua requirement tidak terpenuhi, maka akan ke else dan menjalankan line codenya
 }
