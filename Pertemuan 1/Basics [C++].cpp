c2a9db1ab846b8186282f2af9ce95b60b266f7a5
YasminAssyifa <yasminassyifa2007@gmail.com>
2025-05-25 12:29:32 +0700

    Pertemuan 1 : Pengenalan & Operasi Input/Output

--- Pertemuan 1/Basics [C++].cpp
+++ Pertemuan 1/Basics [C++].cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 int main()
 {
-    string nama;
-    char kom, JenisKelamin;
-    int nim;
-    float ip;
+    string nama; // variabel dengan tipe data teks / kumpulan karakter
+    char kom, JenisKelamin; // variabel dengan tipe data karakter
+    int nim; // variabel dengan tipe data bilangan bulat
+    float ip; // variabel dengan tipe data bilangan desimal
 
-    cout<< "Hello World" << endl;
+    cout<< "Hello World" << endl; 
+    
+    // cout digunakan untuk memberikan output line "hello world"
+    // cin digunakan untuk mengambil inputan user yang dimasukkan kedalam suatu variabel
 
     cout << "Masukkan nama anda : "; 
     // cin >> nama;
-    getline(cin, nama);
+    getline(cin, nama); // untuk mengambil inputan termasuk spasi
 
     cout << "Masukkan KOM anda : "; 
     cin >> kom;
@@ -27,12 +30,12 @@ int main()
     cout << "Masukkan jenis kelamin (L/P) : ";
     JenisKelamin = getche(); //agar karakter langsung tampil tanpa harus menekan enter
     
-    cout << "\n" << nama << endl;
-    cout << kom << endl;
-    cout << nim << endl;
-    cout << ip << endl;
+    cout << "\n" << nama << endl; // "\n" digunakan untuk new line lalu di lanjutkan dengan output variabel nama
+    cout << kom << endl; // output dari variabel kom
+    cout << nim << endl; //output dari variabel nim
+    cout << ip << endl; // output dari variabel ip
     putchar(JenisKelamin); //untuk menampilkan karakter
 
-    getch();
+    getch(); //untuk membaca inputan karakter dr keyboard
 
 }
